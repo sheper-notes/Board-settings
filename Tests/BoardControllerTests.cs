a81f7b7028351013d9c694be05150107641b2a91
flexiconn <simoncox2003@gmail.com>
2023-12-19 14:39:16 +0100

    added mock queries

---
+++ Tests/BoardControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using API.Controllers;
+using IdGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Mock;
+
+namespace Tests
+{
+    public class BoardControllerTests
+    {
+        [Fact]
+        public void GetBoards()
+        {
+            var idGenerator = new IdGenerator(123);
+            var boardQueries = new MockBoardQueries();
+            var boardController = new BoardController();
+        }
+    }
+}
---
+++ Tests/Mock/MockBoardQueries.cs
@@ -0,0 +1,48 @@
+﻿using Common.Enums;
+using Common.Interfaces;
+using Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Mock
+{
+    public class MockBoardQueries : IBoardQueries
+    {
+        private List<Board> boards;
+        private bool CreateBoardSuccesful;
+        public MockBoardQueries(List<Board> boards) {
+            this.boards = boards;
+        }
+
+        public async Task<Board> GetBoardById(long Id)
+        {
+            return boards.Where(x => x.Id == Id).FirstOrDefault();
+        }
+
+        public async Task UpdateBoardName(long Id, string NewName)
+        {
+            boards.Where(x => x.Id == Id).FirstOrDefault().Name = NewName;
+        }
+        public async Task<IEnumerable<Board>> GetBoardsForUser(string Id)
+        {
+            return boards.Where(x => x.Users.Any(y => y.UserId == Id)).ToList();
+
+        }
+        public async Task<bool> CreateBoard(Board board)
+        {
+            if (CreateBoardSuccesful == true) {
+                boards.Add(board);
+                return true;
+            }
+            return false;
+        }
+
+        public async Task UpdateSubscriptionType(long Id, SubscriptionType subscriptionType)
+        {
+
+        }
+    }
+}
